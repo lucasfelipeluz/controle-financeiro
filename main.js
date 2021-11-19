1713938ef412b2c84a49dde499026ce77e52fbb5
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2021-11-19 20:55:26 -0300

    adicionado suporte a virgula

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const form = document.querySelector('#form')
 const inputNomeTransacao = document.querySelector('#text')
 const inputValorTransacao = document.querySelector('#amount')
 const svg = '<svg fill="#00" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="20px" height="20px"><path d="M 10.3125 -0.03125 C 8.589844 -0.03125 7.164063 1.316406 7 3 L 2 3 L 2 5 L 6.96875 5 L 6.96875 5.03125 L 17.03125 5.03125 L 17.03125 5 L 22 5 L 22 3 L 17 3 C 16.84375 1.316406 15.484375 -0.03125 13.8125 -0.03125 Z M 10.3125 2.03125 L 13.8125 2.03125 C 14.320313 2.03125 14.695313 2.429688 14.84375 2.96875 L 9.15625 2.96875 C 9.296875 2.429688 9.6875 2.03125 10.3125 2.03125 Z M 4 6 L 4 22.5 C 4 23.300781 4.699219 24 5.5 24 L 18.59375 24 C 19.394531 24 20.09375 23.300781 20.09375 22.5 L 20.09375 6 Z M 7 9 L 8 9 L 8 22 L 7 22 Z M 10 9 L 11 9 L 11 22 L 10 22 Z M 13 9 L 14 9 L 14 22 L 13 22 Z M 16 9 L 17 9 L 17 22 L 16 22 Z"/></svg>'
-divAlerta = document.querySelector('.alerta');
+const divAlerta = document.querySelector('.alerta');
 
 const transacoesNoArmazenamentoLocal = JSON.parse(localStorage.getItem('transacoes'))
 
@@ -17,13 +17,12 @@ const funcoesDOM = {
     
     const operador = transacoes.valor < 0 ? '-' : ''
     const ClasseCSS = transacoes.valor < 0 ? 'minus' : 'plus'
-    const valorComOperador = Math.abs(transacoes.valor)
-    const li = document.createElement('li')
-
+    const li = document.createElement('li');
+    const displayValor = utilidades.ajustarValor(transacoes.valor, true)
     li.classList.add(ClasseCSS, 'transacao-filho')
     li.innerHTML = `
         <p> ${transacoes.nome} </p>
-        <span class="valor"> R$ ${operador}${valorComOperador}</span>
+        <span class="valor"> R$ ${operador}${displayValor}</span>
         <button class="delete-btn" data-btn-visibilidade="false"
         onclick="armazenamento.remover(${transacoes.id})">
         ${svg}
@@ -49,17 +48,17 @@ const funcoesDOM = {
         .toFixed(2))
 
 
-    DivBalanco.innerHTML = `<span>R$</span> ${total}`
-    DivReceita.textContent = `R$ ${receitas}`
-    DivDespesa.textContent = `R$ ${despesas}`
+    DivBalanco.innerHTML = `<span>R$</span> ${utilidades.ajustarValor(total, false)}`
+    DivReceita.textContent = `R$ ${utilidades.ajustarValor(receitas, false)}`
+    DivDespesa.textContent = `R$ ${utilidades.ajustarValor(despesas, false)}`
   },
 
   lidandoComForm(evento) {
     
     evento.preventDefault();
 
-    const nomeTransacao = inputNomeTransacao.value.trim()
-    const valorTransacao = inputValorTransacao.value.trim()
+    const nomeTransacao = inputNomeTransacao.value.trim();
+    const valorTransacao = utilidades.validandoValorForm(inputValorTransacao.value.trim())
 
     const checarSeInputEstaVazio = inputNomeTransacao.value.trim() === '' || inputValorTransacao.value.trim() === ''
     if(checarSeInputEstaVazio){
@@ -141,6 +140,46 @@ const alertaDOM = {
         divAlerta.dataset.visivel = 'false'
         clearInterval(tempo)
     }, 4000)
+  }
+}
+
+const utilidades = {
+  ajustarValor(valor, removerSinal) {
+    if (removerSinal == true) {
+      valor = Math.abs(valor)
+        .toFixed(2)
+        .toString()
+      
+      if (valor.includes('.')) valor = valor.replace('.', ',')
+      return valor;
+      
+    } else {
+      console.log(valor);
+      valor = valor
+        .toString()
+
+      if (valor.includes('.')) valor = valor.replace('.', ',')
+      return valor;
+
+    }
+  },
+
+  validandoValorForm(valor) {
+    valor = valor.toString();
+
+    if (valor.includes(',')) {
+      valor = valor.toString()
+        .replace(',', '.')
+
+      return parseFloat(valor);
+      
+    } else {
+
+      return parseFloat(valor);
+
+    }
+
+    
   }
 }
 
